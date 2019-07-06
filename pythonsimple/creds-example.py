8281e4592e44e0667203dc21479ba4ca59075e3f
David Glebagit <dgleba@gmail.com>
2019-07-06 09:58:09 -0500

    2019-07-06c

---
+++ pythonsimple/creds-example.py
@@ -0,0 +1,6 @@
+# https://stackoverflow.com/questions/5055042/whats-the-best-practice-using-a-settings-file-in-python
+a = dict(
+  secretkey   = '543210987',
+  dbspec      = 'mysql://user:pass@localhost/prodrptdb',
+  csalt       = "ATaAEGj",
+)
--- pythonsimple/creds-exmple.py
+++
@@ -1,6 +0,0 @@
-# https://stackoverflow.com/questions/5055042/whats-the-best-practice-using-a-settings-file-in-python
-a = dict(
-  secretkey = '543210987',
-	dbspec    = 'mysql://user:pass@localhost/prodrptdb',
-	csalt     = "ATaAEGj",
-)
